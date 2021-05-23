c6886a574ede19d4411aa74e2ca41d58e3ddbb3a
sashademik-1 <alexdid92@gmail.com>
2021-05-23 21:36:21 +0300

    translate to russian

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main() {
     std::string name;
-    std::cin >> name;//your name
+    std::cin >> name;//твое имя
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
