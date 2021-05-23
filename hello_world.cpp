3b687311692d175d4e06b4236c26aec45ff6821b
sashademik-1 <alexdid92@gmail.com>
2021-05-23 19:38:48 +0300

    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main() {
     std::string name;
-    std::cin >> name;
+    std::cin >> name;//your name
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
